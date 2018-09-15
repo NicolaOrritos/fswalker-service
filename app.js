6c87d8d170de12f5bd7bc13328e9c0f7f08d006e
Nicola Orritos <nicola.orritos@gmail.com>
2018-09-15 23:32:11 +0200

    Merged the business logic into the microservice code

--- app.js
+++ app.js
@@ -5,15 +5,21 @@
 require('dotenv').config()
 
 const restify = require('restify')
-// const errors  = require('restify-errors')
+const errors  = require('restify-errors')
 const getenv  = require('getenv')
+const path    = require('path')
+const url     = require('url')
 
 const status  = require('./lib/status')
 const after   = require('./lib/after')
 
+const {walk}  = require('./lib/walk')
+
 
 const HOST  = getenv('HOST')       || '0.0.0.0'
 const PORT  = getenv.int('PORT')   || 8080
+
+const BASE_PATH = getenv('BASE_PATH') || '.'
 
 
 const pkg = require('./package.json')
@@ -21,22 +27,41 @@ const pkg = require('./package.json')
 
 const server = restify.createServer({name: pkg.name})
 
-server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
 
 
 server.get('/status', status)
 
 
-server.get('/fs/*', (req, res, next) =>
+server.get('/fs/*', async (req, res, next) =>
 {
-    const path = req.url.slice('/fs/'.length)
+    try
+    {
+        const request_url = url.parse(req.url, true)
 
-    console.log(`Asked for path "${path}"`)
+        let folder = request_url.pathname.slice('/fs/'.length)
+        const {filterout} = request_url.query
 
-    res.send('Hello ' + path)
+        console.log(`Asked for path "${folder}" with filter "${filterout}"`)
 
-    next()
+        folder = folder.replace(/\./g,   '')
+        folder = folder.replace(/\.\./g, '')
+        folder = folder.replace(/\/\//g, '/')
+
+        folder = path.join(BASE_PATH, folder)
+
+        console.log(`Resolved to "${folder}"`)
+
+        const paths = await walk(folder, filterout)
+
+        res.send(paths)
+
+        next()
+    }
+    catch (err)
+    {
+        next(new errors.InternalServerError(err))
+    }
 })
 
 
